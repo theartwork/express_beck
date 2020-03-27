b32e5fbed955a661986a8b095057a07a5a6ff131
theartwork <info@theartwork.space>
2020-03-27 13:29:23 +0100

    first

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',function(anfrage,antwort){
+    antwort.send('Hello from the serve side!');    
+});
+app.listen(80);
+console.log(`Server running, listening on port: 80`);
