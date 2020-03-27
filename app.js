93de5cdf8148d752692b6e582e1df6579bd19d75
theartwork <info@theartwork.space>
2020-03-27 13:38:27 +0100

    spaces

--- app.js
+++ app.js
@@ -6,5 +6,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/',function(anfrage,antwort){
     antwort.send('Hello from the serve side!');    
 });
+
 app.listen(80);
+
 console.log(`Server running, listening on port: 80`);
